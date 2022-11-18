b5685d972cb43b8147dcd70c06d0385d1698a983
Saba <saba.feilizadeh@gmail.com>
2022-11-18 15:04:43 -0800

    add the addNote functionality.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -6,11 +6,23 @@ import CreateArea from "./CreateArea";
 
 
 function App(){
+
+    const [notes, setNotes] = useState([]);
+
+    function createNote(newNote){
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+        console.log(notes);
+    }
+
     return (
         <div>
             <Header />
-            <CreateArea />
-            <Note key={1} title="Note title" content="Note content" />
+            <CreateArea onAdd={createNote}/>
+            {notes.map(note => {
+                return <Note title={note.title} content={note.content} />
+            })}
             <Footer />
         </div>
     );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+    const [note, setNote] = useState({
+        title: "",
+        content:""
+    });
+
+    function updateNote(event){
+        const {name, value} = event.target;
+        setNote(prevNotes => {
+            return {
+                ...prevNotes,
+                [name]: value
+            }
+        });
+    }
+
+    function passNote(event){
+        props.onAdd(note);
+        setNote({title: "", content:""});
+        //by default when the add button is clicked, it refreshes the page
+        event.preventDefault();
+    }
+
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input 
+            onChange={updateNote} 
+            name="title" 
+            value={note.title} 
+            placeholder="Title"
+        />
+
+        <textarea 
+            onChange={updateNote} 
+            name="content" 
+            value={note.content} 
+            placeholder="Take a note..." 
+            rows="3"
+        />
+
+        <button onClick={passNote}>Add</button>
       </form>
     </div>
   );
